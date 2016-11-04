a6e7e94ee8c8ab8322d9eedbcdae04723a626520
vmaicon <vmaicon.araujo@gmail.com>
2016-11-04 14:48:07 -0500

    alteração para senha padrão

--- src/java/dao/ConnectionFactory.java
+++ src/java/dao/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection(
-            		"jdbc:mysql://localhost/agenda", "root", "mysql@2015");
+            		"jdbc:mysql://localhost/agenda", "root", "root");
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
